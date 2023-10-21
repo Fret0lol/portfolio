a3ec9240f1914f35609b8dcb2aa0ab8756cbc1fc
Fret <clemfret7@gmail.com>
2023-10-21 14:19:25 +0200

    add cursor effect

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Cv from './components/Cv.vue';
+import Cursor from './components/Cursor.vue';
 </script>
 
 <template>
   <Cv></Cv>
+  <Cursor />
 </template>
 
 <style scoped>
---
+++ src/components/Cursor.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {gsap} from "gsap";
+import {onMounted} from "vue";
+
+onMounted(() => {
+	gsap.set(".cursor", {xPercent: -50, yPercent: -50});
+
+	let xTo = gsap.quickTo(".cursor", "x", {duration: 0.6, ease: "power3"}),
+		yTo = gsap.quickTo(".cursor", "y", {duration: 0.6, ease: "power3"});
+
+	window.addEventListener("mousemove", (e) => {
+		xTo(e.clientX);
+		yTo(e.clientY);
+	});
+});
+</script>
+<template>
+	<div class="cursor"></div>
+</template>
+<style lang="scss" scoped>
+.cursor {
+	width: 35px;
+	height: 35px;
+	position: fixed;
+	top: 0;
+	left: 0;
+  background-color: #5fb3a1;
+	border-radius: 50%;
+	pointer-events: none;
+}
+</style>
